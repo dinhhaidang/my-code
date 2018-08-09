e1034da569e729ecb471b007351b8b38fab912ec
Dinh Hai Dang <dinhaidang@gmail.com>
2018-08-09 14:17:43 +0700

    add

--- build_k8s_jenkins.sh
+++ build_k8s_jenkins.sh
@@ -35,6 +35,9 @@ kubectl create serviceaccount --namespace kube-system tiller
 kubectl create clusterrolebinding tiller-cluster-rule --clusterrole=cluster-admin --serviceaccount=kube-system:tiller
 kubectl patch deploy --namespace kube-system tiller-deploy -p '{"spec":{"template":{"spec":{"serviceAccount":"tiller"}}}}'
 
+#tao storage class ssd cho k8s
+kubectl apply -f ssd_storageclass.yaml
+
 
 echo "#-------------------------------------------------------------------#"
 echo "#         Chuan bi setup jenkins, se bat dau trong 1 phut...        #"
