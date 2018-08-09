82bc98f2c7e933d77b2983e7448ac2fecc2a9099
Dinh Hai Dang <dinhaidang@gmail.com>
2018-08-09 09:33:48 +0700

    add

--- uninstall_k8s_jenkins.sh
+++ uninstall_k8s_jenkins.sh
@@ -8,5 +8,4 @@ gcloud container clusters delete demo-jenskin
 echo "#-------------------------------------------------------------------#"
 echo "#                   Xoa source repositories                         #"
 echo "#-------------------------------------------------------------------#"
-gcloud init
 gcloud source repos delete demo-jenkins
