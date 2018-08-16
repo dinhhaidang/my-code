550f00202a6df42da8ae0296937049804ee266a0
Dinh Hai Dang <dinhaidang@gmail.com>
2018-08-16 14:08:43 +0700

    add

--- 5.re-new-services.sh
+++ 5.re-new-services.sh
@@ -8,3 +8,15 @@ kubectl --namespace=production apply -f k8s/services
 kubectl --namespace=production scale deployment gceme-frontend-production --replicas=4
 kubectl --namespace=production get service gceme-frontend
 
+
+printf "y/n" | gcloud source repos delete demo-jenkins || true
+
+gcloud source repos create demo-jenkins
+git init
+git config credential.helper gcloud.sh
+git remote add origin https://source.developers.google.com/p/vn-cloudace-dataengine-2018/r/demo-jenkins
+git config --global user.email "dinh@cloud-ace.com"
+git config --global user.name "dinh@cloud-ace.com"
+git add .
+git commit -m "Initial commit"
+git push origin master
