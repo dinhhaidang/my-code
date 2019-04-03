74c89acfd738a774b1b977f5401d25e7872a1229
dinhhaidang <dinhaidang@gmail.com>
2019-04-03 11:21:01 +0700

    Update 1.build_k8s_jenkins.sh

--- 1.build_k8s_jenkins.sh
+++ 1.build_k8s_jenkins.sh
@@ -97,8 +97,8 @@ gcloud source repos create demo-jenkins
 git init
 git config credential.helper gcloud.sh
 git remote add origin https://source.developers.google.com/p/ecoe-k8s/r/demo-jenkins
-git config --global user.email "dinh@cloud-ace.com"
-git config --global user.name "dinh@cloud-ace.com"
+git config --global user.email "dang.winapp@gmail.com"
+git config --global user.name "dang.winapp@gmail.com"
 git add .
 git commit -m "Initial commit"
 git push origin master
